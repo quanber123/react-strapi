42bc38050a1f148a8a5f3f656cd00d9b439fe861
quanber123 <quanber123@icloud.com>
2023-06-05 14:02:30 +0700

    create react + strapi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
 function App() {
-  const [blogs, setBlogs] = useState([]);
-  useEffect(() => {
-    const axiosData = async () => {
-      try {
-        const res = await axios.get('http://localhost:1337/api/blogs');
-        const { data } = res.data;
-        setBlogs(data);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    axiosData();
-  }, []);
+  const [count, setCount] = useState(0)
+
   return (
-    <section>
-      {blogs.map(({ id, attributes }) => {
-        return <li key={id}>{attributes.blogTitle}</li>;
-      })}
-    </section>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
